0b559778b2836875e3e6dca3d5b54995e4e8fd3b
malek salem <malek.salem.14@gmail.com>
2021-11-03 21:57:57 +0100

    Modify notifications

--- 0x02-react_intro/task_2/dashboard/src/utils.js
+++ 0x02-react_intro/task_2/dashboard/src/utils.js
@@ -2,7 +2,11 @@ export function getFullYear() {
     return new Date().getFullYear();;
   };
   
-  export function getFooterCopy(isIndex) {
+export function getFooterCopy(isIndex) {
     if(isIndex) return "Holberton School";
     return "Holberton School main dashboard";
+  };
+
+export function getLatestNotification() {
+    return "<strong>Urgent requirement</strong> - complete by EOD"
   };
